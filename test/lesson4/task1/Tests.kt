8e8d3af7a73cfd5ac48c366a586f257cadd83b8b
anuta <anutajjk@gmail.com>
2019-11-24 04:07:33 +0300

    задачи lesson 4 - 5

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -230,16 +230,17 @@ class Tests {
         assertEquals("DCXCIV", roman(694))
         assertEquals("XLIX", roman(49))
     }
-}
 
-@Test
-@Tag("Impossible")
-fun russian() {
-    assertEquals("триста семьдесят пять", russian(375))
-    assertEquals("двадцать две тысячи девятьсот шестьдесят четыре", russian(22964))
-    assertEquals("сто девятнадцать тысяч пятьсот восемь", russian(119508))
-    assertEquals("две тысячи три", russian(2003))
-    assertEquals("двести тысяч два", russian(200002))
-    assertEquals("девятьсот тысяч", russian(900000))
-    assertEquals("двенадцать", russian(12))
+
+    @Test
+    @Tag("Impossible")
+    fun russian() {
+        assertEquals("триста семьдесят пять", russian(375))
+        assertEquals("двадцать две тысячи девятьсот шестьдесят четыре", russian(22964))
+        assertEquals("сто девятнадцать тысяч пятьсот восемь", russian(119508))
+        assertEquals("две тысячи три", russian(2003))
+        assertEquals("двести тысяч два", russian(200002))
+        assertEquals("девятьсот тысяч", russian(900000))
+        assertEquals("двенадцать", russian(12))
+    }
 }
